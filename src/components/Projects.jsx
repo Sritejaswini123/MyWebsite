9f947f0d3a8056e55ce1d90402ced263eefae969
Sritejaswini123 <Sritejaswini1234@gmail.com>
2025-06-23 11:03:37 +0530

    added image path

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,37 +1,43 @@
 // Projects.jsx
 import React from 'react';
 import './Projects.css';
+// About.jsx
+import AnediumImage from '../assets/Anedium-frontpage.jpeg';
+import MealsAppImage from '../assets/Meals App.png';
+import NetflixGptImage from '../assets/netflix.jpg';
+import RestaurantSystemImage from '../assets/REST.webp';
+import JobPortalImage from '../assets/job-portal.jpg';
 
 const Projects = () => {
   const projects = [
     {
         title: "Anedium Application",
         description: "Developed a real-time mobile application from scratch using React Native CLI, featuring professional UI design, Firebase authentication, REST API integration, reusable components, and deployment-ready builds for Play Store launch.",
-        image: "./src/assets/Anedium-frontpage.jpeg",
+        image: AnediumImage,
         githubLink: "https://github.com/yourusername/code-geass"
       },
       {
         title: "Meals App",
         description: "Developed the 'Meals App' using React Native, enabling users to discover, explore, and manage recipes with an intuitive and user-friendly interface.",
-        image: "./src/assets/Meals App.png",
+        image: MealsAppImage,
         githubLink: "https://github.com/Sritejaswini123/MEALS-APP-USING-REACT-NATIVE"
       },
     {
       title: "Netflix Gpt",
       description: "Developed Netflix GPT, a dynamic web app utilizing HTML, Tailwind CSS, React.js, Redux, Firebase, and REST API calls to deliver seamless user authentication, personalized content integration, and optimized navigation for enhanced user engagement.",
-      image: "./src/assets/netflix.jpg",
+      image: NetflixGptImage,
       githubLink: "https://github.com/Sritejaswini123/NetflixGPT"
     },
     {
       title: "Restaurant System - Book your spot",
       description: "Designed and developed a Restaurant Management System website using CSS3 properties like background, flex, and box model, while seamlessly integrating front-end and back-end systems through cross-functional collaboration.",
-      image: "./src/assets/REST.webp",
+      image: RestaurantSystemImage,
       githubLink: "https://github.com/Sritejaswini123/RESTAURANT-WEBSITE"
     },
     {
       title: "Career Startup - Online Job Portal",
       description: "Orchestrated the development of the Career Startup Online Job Portal using HTML, CSS, JavaScript, PHP, and MySQL to address students' job search challenges with efficient backend integration and database management.",
-      image: "./src/assets/job-portal.jpg",
+      image: JobPortalImage,
       githubLink: "https://github.com/Sritejaswini123/JOB-SEARCH-AND-RECURITMENT-PLLATFORM"
     },
    
