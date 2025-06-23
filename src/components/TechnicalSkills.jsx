c5914af2bc6dbac5b1884fd21b2ff714fec80476
Sritejaswini123 <Sritejaswini1234@gmail.com>
2025-06-23 09:47:28 +0530

    add skills and experience

--- src/components/TechnicalSkills.jsx
+++ src/components/TechnicalSkills.jsx
@@ -11,22 +11,28 @@ const TechnicalSkills = () => {
           <div className="skills-content">
             <div className="skill-group">
               <span className="skill-label">Programming Language:</span>
-              <span className="skill-items">C, C++, Java, JavaScript,  Python</span>
+              <span className="skill-items">C, C++, Java, JavaScript,Python, TypeScript</span>
             </div>
             
             <div className="skill-group">
               <span className="skill-label">Frameworks/ Libraries:</span>
-              <span className="skill-items">Springboot,React.js, React Native Using Javascript , CSS,Html, REST APIs  </span> 
+              <span className="skill-items">Springboot,React.js, React Native Using Javascript , CSS,Html, REST APIs , React Native</span> 
             </div>
+
+                <div className="skill-group">
+              <span className="skill-label">Backend:</span>
+              <span className="skill-items"> Hono , Drizzle ORM , Bruno, Avien, Better-auth, Postman </span> 
+            </div>
+            
             
             <div className="skill-group">
               <span className="skill-label">Database:</span>
-              <span className="skill-items">MySQL,Postman</span>
+              <span className="skill-items">MySQL,Postgresql</span>
             </div>
 
             <div className="skill-group">
               <span className="skill-label">Developer Tools:</span>
-              <span className="skill-items"> Vs code , eclispe, Spring Tool Suite, SQL Workbench, Postman, Android Studio</span>
+              <span className="skill-items"> Vs code , eclispe, Spring Tool Suite, SQL Workbench, Android Studio</span>
             </div>
           </div>
         </div>
